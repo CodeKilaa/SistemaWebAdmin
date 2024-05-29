7f6d15fe8d3afcdff3a1fada428a7ccd64ae568b
CodeKilaa <xluixarturox123@gmail.com>
2024-05-29 14:07:43 -0500

    fix navbar- add style sidebar

--- sol_ventasweb/CapaDatos/Conexion.cs
+++ sol_ventasweb/CapaDatos/Conexion.cs
@@ -8,6 +8,6 @@ namespace CapaDatos
 {
     public class Conexion
     {
-        public static string CN = "Data Source=.;Initial Catalog=DBPRUEBAS;Integrated Security=True";
+        public static string CN = "Data Source=.;Initial Catalog=DBPRUEBAS;User ID=sa;Password=123456;Encrypt=False";
     }
 }
