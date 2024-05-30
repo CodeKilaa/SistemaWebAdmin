08780ae2e8b63c823099deadf6ab4300040ebe87
ranp20 <polancoranp2019@gmail.com>
2024-05-30 09:47:03 -0500

    merge my changes with changes into branch luis

--- sol_ventasweb/CapaDatos/Conexion.cs
+++ sol_ventasweb/CapaDatos/Conexion.cs
@@ -8,6 +8,7 @@ namespace CapaDatos
 {
     public class Conexion
     {
-        public static string CN = "Data Source=.;Initial Catalog=DBPRUEBAS;Integrated Security=True";
+        // public static string CN = "Data Source=.;Initial Catalog=DBPRUEBAS;Integrated Security=True";
+        public static string CN = "Data Source=DESKTOP-G46KIE0\\SQLEXPRESS;Database=DBPRUEBAS;User ID=sa;Password=123456;Trusted_Connection=false;TrustServerCertificate=True;";
     }
 }
